1fdfffabd25a26df6f106d9ab2528b196de80fa3
Rajeswari Nag <rajeswarinag451@gmail.com>
2024-09-28 11:22:05 +0530

    Create SpringSecurity111Application.java

---
+++ SpringSecurity111Application.java
@@ -0,0 +1,14 @@
+package com.spring.web.SpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity111Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity111Application.class, args);
+		System.out.print("Welcome to springboot");
+	}
+
+}
